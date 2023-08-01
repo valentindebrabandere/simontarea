eae7108dc9ec1261eed7f2ad98cde59cd7db8f51
Valentin de Brabandère <valentindebrabandere@gmail.com>
2023-08-01 16:40:21 +0200

    gallery btn working

--- dist/assets/js/main.bundle.js
+++ dist/assets/js/main.bundle.js
@@ -28,22 +28,32 @@
         const parent = gallery.closest(".c-cardsection");
         parent.classList.add("c-cardsection--dyn");
         gallery.classList.add("c-card-gallery--dyn");
-        const nextButton = document.querySelector(".c-cardsection__btn.next");
-        const prevButton = document.querySelector(".c-cardsection__btn.prev");
         const footer = parent.querySelector(".c-cardsection__footer");
+        const prevButton = footer.querySelector(".c-cardsection__btn.prev");
+        const nextButton = footer.querySelector(".c-cardsection__btn.next");
+        let galleryWidth = gallery.offsetWidth;
+        let currentScrollPosition = 0;
+        let cardWidth = 400;
+        cardGallery.forEach((card, index) => {
+          cardWidth = card.offsetWidth;
+          const deco = card.querySelector(".c-card__deco");
+          const direction = index % 2 === 0 ? -1 : 1;
+          deco.setAttribute("data-rotate", 10 * -direction + ", " + direction);
+          console.log("done");
+        });
         nextButton.addEventListener("click", () => {
-          parent.scrollLeft += 400;
+          let scrollOffset = cardWidth + 20;
+          let potentialScrollPosition = currentScrollPosition + cardWidth + 20;
+          currentScrollPosition = Math.min(potentialScrollPosition, galleryWidth - scrollOffset);
+          gallery.style.transform = `translate3d(-${currentScrollPosition}px, 0, 0)`;
         });
         prevButton.addEventListener("click", () => {
-          parent.scrollLeft -= 400;
+          let potentialScrollPosition = currentScrollPosition - cardWidth - 20;
+          currentScrollPosition = Math.max(potentialScrollPosition, 0);
+          gallery.style.transform = `translate3d(-${currentScrollPosition}px, 0, 0)`;
         });
         footer.style.display = "flex";
       }
-      cardGallery.forEach((card, index) => {
-        const deco = card.querySelector(".c-card__deco");
-        const direction = index % 2 === 0 ? -1 : 1;
-        deco.setAttribute("data-rotate", 10 * -direction + ", " + direction);
-      });
     });
   };
 
--- src/assets/js/modules/init-cards.js
+++ src/assets/js/modules/init-cards.js
@@ -1,3 +1,5 @@
+//init-cards.js
+
 const allCardGalleries = document.querySelectorAll('.c-card-gallery');
 
 const init = () => {
@@ -11,27 +13,38 @@ const init = () => {
             parent.classList.add('c-cardsection--dyn');
             gallery.classList.add('c-card-gallery--dyn');
 
-            const nextButton = document.querySelector('.c-cardsection__btn.next');
-            const prevButton = document.querySelector('.c-cardsection__btn.prev');
             const footer = parent.querySelector('.c-cardsection__footer');
+            const prevButton = footer.querySelector('.c-cardsection__btn.prev');
+            const nextButton = footer.querySelector('.c-cardsection__btn.next');
 
-            nextButton.addEventListener('click', () => {
-                parent.scrollLeft += 400; // adjust the value as needed
+            let galleryWidth = gallery.offsetWidth;
+            let currentScrollPosition = 0;
+            let cardWidth = 400;
+
+            cardGallery.forEach((card, index) => {
+                cardWidth = card.offsetWidth;
+                const deco = card.querySelector('.c-card__deco');
+                const direction = index % 2 === 0 ? -1 : 1;
+                deco.setAttribute('data-rotate', 10* -direction + ', ' + direction);
+                console.log("done")
             });
 
+            nextButton.addEventListener('click', () => {
+                let scrollOffset = cardWidth + 20;
+                let potentialScrollPosition = currentScrollPosition + cardWidth + 20;
+                currentScrollPosition = Math.min(potentialScrollPosition, galleryWidth - scrollOffset);
+                gallery.style.transform = `translate3d(-${currentScrollPosition}px, 0, 0)`;
+            });
+            
             prevButton.addEventListener('click', () => {
-                parent.scrollLeft -= 400; // adjust the value as needed
+                let potentialScrollPosition = currentScrollPosition - cardWidth - 20;
+                currentScrollPosition = Math.max(potentialScrollPosition, 0);
+                gallery.style.transform = `translate3d(-${currentScrollPosition}px, 0, 0)`;
             });
 
             // Show the footer only when there are more than two items
             footer.style.display = "flex";
         }
-
-        cardGallery.forEach((card, index) => {
-            const deco = card.querySelector('.c-card__deco');
-            const direction = index % 2 === 0 ? -1 : 1;
-            deco.setAttribute('data-rotate', 10* -direction + ', ' + direction);
-        });
     });
 };
 
