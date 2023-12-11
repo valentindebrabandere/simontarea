05cf99dd9d02cce4d9652193a8050d4609b1bcbc
Valentin de Brabandère <valentindebrabandere@gmail.com>
2023-12-11 11:18:49 +0100

    a lot of small changes

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,18 @@
+// plugins
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+// collections
 const schema = require("./src/_11ty/collections/schema.js");
 
+// filters
+const limit = require("./src/_11ty/filters/limit.js");
+const dates = require("./src/_11ty/filters/dates.js");
+
 module.exports = function (eleventyConfig) {
-  // copy
-  eleventyConfig.addPassthroughCopy("./src/assets/img");
-  eleventyConfig.addPassthroughCopy("./src/assets/fonts");
-  eleventyConfig.addPassthroughCopy({ "./src/static": "./" });
 
   // collections
   const allItems = (collectionApi) => {
     return schema(collectionApi).schema__items;
-
   };
   eleventyConfig.addCollection("schema__bigItems", (collectionApi) => {
     return schema(collectionApi).schema__bigItems;
@@ -18,12 +21,36 @@ module.exports = function (eleventyConfig) {
     return schema(collectionApi).schema__smallItems;
   });
 
-  // shortcodes
-  eleventyConfig.addNunjucksShortcode("consoleLog", function (data) {
-    return `<script>console.log(${JSON.stringify(data)});</script>`;
+  // filters
+  eleventyConfig.addFilter("limit", limit);
+  eleventyConfig.addFilter("dateISO", dates.dateISO);
+  eleventyConfig.addFilter("dateFeed", dates.dateFeed);
+  eleventyConfig.addFilter("dateFull", dates.dateFull);
+  eleventyConfig.addFilter("dateFormat", dates.dateFormat);
+  eleventyConfig.addFilter("dateYear", dates.dateYear);
+
+  // plugins
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    trim: true,
   });
 
-  // override base config
+  // ignores
+  eleventyConfig.ignores.add("src/assets/**/*");
+  eleventyConfig.watchIgnores.add("src/assets/**/*");
+
+  // passthrough copy
+  eleventyConfig.setServerPassthroughCopyBehavior("copy");
+  eleventyConfig.addPassthroughCopy({ "./src/static": "/" });
+  eleventyConfig.addPassthroughCopy("./src/assets/img");
+  eleventyConfig.addPassthroughCopy("./src/assets/fonts");
+
+  // server config
+  eleventyConfig.setServerOptions({
+    watch: ["./dist/assets/css/**/*.css", "./dist/assets/js/**/*.js"],
+    port: 8000,
+  });
+
+  // base config
   return {
     dir: {
       input: "src",
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,9 @@
 module.exports = {
+  title: "Simon Tarea",
+  description: "Simon Tarea personnal website",
   // TODO Change me to the final domain name
   url: "https://simon-tarea.netlify.app",
-  title: "Simon Tarea",
+  baseUrl: "/",
+  author: "Valentin de Brabandère",
+  buildTime: new Date(),
 };
